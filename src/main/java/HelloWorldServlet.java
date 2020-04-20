3480674a919bc5cccf048e006e0c61a9e4934288
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-20 15:03:18 -0500

    working on bonuses

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,41 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    private Integer sharedCounter;
+
+    @Override
+    public void init(final ServletConfig config) throws ServletException {
+        super.init(config);
+        getServletContext().log("init() called");
+        sharedCounter = 0;
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        getServletContext().log("service() called");
+
+        int localCounter;
+        synchronized (sharedCounter) {
+            sharedCounter++;
+
+            localCounter = sharedCounter;
+        }
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+        out.println("<h2>This is an h2</h2>");
+        out.println("People Vistied : " + localCounter);
+    }
+
+
 }
