079a27e0e0987f6f37b7f059f079e1ef815d572f
Ted Zamarron <ted.zamarron@gmail.com>
2020-04-20 16:33:34 -0500

    modifications to code and added count

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -1,4 +1,3 @@
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -6,19 +5,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        String name = req.getParameter("name");
-        if (name != null ) {
-            out.println("<h1>Hello " + name + "!</h1>");
+        int pageCounter = 0;
+        String counter = req.getParameter("count");
+        if (counter != null ) {
+            pageCounter = 0;
+            out.println("<p>Number of times this page has been viewed: "+pageCounter);
         } else {
-            out.println("<h1>Hello World!</h1>");
+            pageCounter++;
+            out.println("<p>Number of times this page has been viewed: "+pageCounter);
         }
     }
-
-
 }
